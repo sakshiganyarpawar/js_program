e419e3945e2cf5f06db5d4e8a99a6a5c60e0dce1
sakshiganyarpawar <sakshiganyarpawar263@gmail.com>
2024-07-10 22:57:21 +0530

    let_keyword_apply_to_variable

--- largest.js
+++ largest.js
@@ -1,6 +1,6 @@
 const nums=[23,45,78,459,90]
 console.log(`Array:${nums}`);
-large_no=nums[0];
+ let large_no=nums[0];
 for(let i=0;i<=4;i++){
     if(nums[i]>=large_no){
          large_no=nums[i];
